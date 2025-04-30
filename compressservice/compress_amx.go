152654b721682e1a24e700d9e33380abe1de8023
hyxxyh57 <77915450+HYX20011209@users.noreply.github.com>
2025-04-30 15:55:30 -0400

    Try amx (#2)
    * first try
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix

---
+++ compressservice/compress_amx.go
@@ -0,0 +1,55 @@
+//go:build amx && amd64
+// +build amx,amd64
+
+package compressservice
+
+/*
+#cgo LDFLAGS: -lrt -ldl
+#include <immintrin.h>
+
+// 轻量 AMX：清零 tile-0
+static inline void amx_touch() {
+    _tile_zero(0);
+    _tile_release();
+}
+*/
+import "C"
+import (
+	"log"
+	"sync"
+	"syscall"
+)
+
+// Linux x86_64 constants
+const (
+	_ARCH_REQ_XCOMP_PERM = 0x1022 // from <asm/prctl.h>
+	_XFEATURE_XTILEDATA  = 18     // tile data component
+)
+
+// tryEnableAMX issues arch_prctl to get AMX permission.
+// Returns true on success.
+func tryEnableAMX() bool {
+	_, _, errno := syscall.RawSyscall(syscall.SYS_ARCH_PRCTL,
+		uintptr(_ARCH_REQ_XCOMP_PERM),
+		uintptr(_XFEATURE_XTILEDATA), 0)
+	return errno == 0
+}
+
+var (
+	hasAMX = true
+	once   sync.Once
+)
+
+func iaaCompress(src []byte) []byte {
+	once.Do(func() {
+		if ok := tryEnableAMX(); !ok {
+			log.Println("AMX not available; falling back to scalar")
+			hasAMX = false
+		}
+	})
+
+	if hasAMX {
+		C.amx_touch() // 触发一次 AMX 指令
+	}
+	return compressScalar(src)
+}
